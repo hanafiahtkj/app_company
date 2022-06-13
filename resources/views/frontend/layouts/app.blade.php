daa8abdafe53c301fd832bdcb0981936cac7b25a
Hanafi <hanafiahtkj@gmail.com>
2022-06-13 15:57:56 +0800

    #

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -1,52 +1,88 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="en">
 
 <head>
-    <meta charset="utf-8" />
-    <link rel="apple-touch-icon" sizes="76x76" href="{{asset('img/favicon.png')}}">
-    <link rel="icon" type="image/png" href="{{asset('img/favicon.png')}}">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <title>@yield('title') | {{ config('app.name') }}</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="{{ setting('meta_description') }}">
-    <meta name="keyword" content="{{ setting('meta_keyword') }}">
+  <meta charset="utf-8">
 
-    @include('frontend.includes.meta')
+  <title>@yield('title') | {{ config('app.name') }}</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="{{ setting('meta_description') }}">
+  <meta name="keyword" content="{{ setting('meta_keyword') }}">
 
-    <!-- Shortcut Icon -->
-    <link rel="shortcut icon" href="{{asset('img/favicon.png')}}">
-    <link rel="icon" type="image/ico" href="{{asset('img/favicon.png')}}" />
+  @include('frontend.includes.meta')
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <!-- Favicons -->
+  <link href="{{ asset('vendor/flexor/assets/img/favicon.png') }}" rel="icon">
+  <link href="{{ asset('vendor/flexor/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
-    @stack('before-styles')
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="stylesheet" href="{{ mix('css/frontend.css') }}">
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  
+  @stack('before-styles')
 
-    @stack('after-styles')
+  <!-- Vendor CSS Files -->
+  <link href="{{ asset('vendor/flexor/assets/vendor/aos/aos.css') }}" rel="stylesheet">
+  <link href="{{ asset('vendor/flexor/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('vendor/flexor/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+  <link href="{{ asset('vendor/flexor/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('vendor/flexor/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('vendor/flexor/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
-    <x-google-analytics />
+  <!-- Template Main CSS File -->
+  <link href="{{ asset('vendor/flexor/assets/css/style.css') }}" rel="stylesheet">
+
+  @stack('after-styles')
+
+  <x-google-analytics />
+
+  <!-- =======================================================
+  * Template Name: Flexor - v4.7.0
+  * Template URL: https://bootstrapmade.com/flexor-free-multipurpose-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
 </head>
 
 <body>
 
     @include('frontend.includes.header')
 
+    <!-- ======= Hero Section ======= -->
+    <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+        <div class="container" data-aos="fade-in">
+        <h1>Welcome to {{ app_name() }}</h1>
+        <h2>We are team of talented designers making websites with Bootstrap</h2>
+        <div class="d-flex align-items-center">
+            <i class="bx bxs-right-arrow-alt get-started-icon"></i>
+            <a href="#about" class="btn-get-started scrollto">Get Started</a>
+        </div>
+        </div>
+    </section><!-- End Hero -->
 
-    <main>
+    <main id="main">
+
         @yield('content')
-    </main>
+
+    </main><!-- End #main -->
 
     @include('frontend.includes.footer')
 
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="{{ asset('vendor/flexor/assets/vendor/aos/aos.js') }}"></script>
+  <script src="{{ asset('vendor/flexor/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('vendor/flexor/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+  <script src="{{ asset('vendor/flexor/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+  <script src="{{ asset('vendor/flexor/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+  <script src="{{ asset('vendor/flexor/assets/vendor/php-email-form/validate.js') }}"></script>
+
+  <!-- Template Main JS File -->
+  <script src="{{ asset('vendor/flexor/assets/js/main.js') }}"></script>
+
 </body>
-
-<!-- Scripts -->
-@stack('before-scripts')
-
-<script src="{{ mix('js/frontend.js') }}"></script>
-
-@stack('after-scripts')
 
 </html>
